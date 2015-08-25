1f93366b7c1f18787a57dcc49736f80f920c98eb
Kevin Richards <kevin.richards@socotra.com>
2015-08-25 13:45:08 -0700

    Create an archive of the guide

--- scripts/build.py
+++ scripts/build.py
@@ -3,6 +3,10 @@ from os.path import isfile, join
 import shutil
 import os
 import subprocess
+from tempfile import mkstemp
+from shutil import move
+from os import remove, close
+import tarfile
 
 def validate_path_exists(path):
 	assert os.path.exists(path), 'Path %s does not exist' % path
@@ -11,10 +15,11 @@ def validate_path_exists(path):
 def get_files_in_dir(path):
 	return filter(lambda f: isfile(join(path, f)), listdir(path))
 
-def create_dist_dir(path):
+def create_dist_dir(path, html_path):
 	if os.path.exists(path):
 		shutil.rmtree(path)
 	os.makedirs(path)
+	os.makedirs(html_path)
 
 def convert_to_html_filename(filename):
 	return filename[:-3] + '.html'
@@ -35,10 +40,35 @@ def grip_file(f, markdown_path, dist_path):
 def grip_files(files, markdown_path, dist_path):
 	return map(lambda f: grip_file(f, markdown_path, dist_path), files)
 
+def replace(file_path, pattern, subst):
+    fh, abs_path = mkstemp()
+    with open(abs_path,'w') as new_file:
+        with open(file_path) as old_file:
+            for line in old_file:
+                new_file.write(line.replace(pattern, subst))
+    close(fh)
+    remove(file_path)
+    move(abs_path, file_path)
+
+def replace_markdown_links(files):
+	for f in files:
+		replace(f, '.md', '.html')
+
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
 if __name__ == "__main__":
 	markdown_path = validate_path_exists('guide')
-	dist_path = 'dist'
-	create_dist_dir(dist_path)
+	dist_dir = 'dist'
+	dist_html_path = 'dist/html'
+	create_dist_dir(dist_dir, dist_html_path)
 	files = get_files_in_dir(markdown_path)
-	gripped_files = grip_files(files, markdown_path, dist_path)
-	print str(gripped_files)
+	print 'found %s in %s' % (files, markdown_path)
+	print 'gripping files'
+	gripped_files = grip_files(files, markdown_path, dist_html_path)
+	print 'replacing markdown links with html links'
+	replace_markdown_links(gripped_files)
+	print 'making archive'
+	make_tarfile(join(dist_dir, 'python_guide.tar.gz'), dist_html_path)
+	print 'complete!'
